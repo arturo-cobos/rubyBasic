dba1394c254b988fd46b2130abbe3f0e8676e1a1
Arturo Cobos <arturo@co-boss.com>
2016-03-09 00:31:08 -0500

    función con argumento variable

--- conversion_de_tipo_de_cambio.rb
+++ conversion_de_tipo_de_cambio.rb
@@ -1,9 +1,8 @@
 
-dolares = 800
 
-def dollar_to_currency(dollars)
-	tipo_de_cambio = 17.77
-	return dolares * tipo_de_cambio
+
+def dollar_to_currency(dollars, tipo_de_cambio = 17.77)
+	return dollars * tipo_de_cambio
 end
 
 puts dollar_to_currency(200)
