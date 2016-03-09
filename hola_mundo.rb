50248bc688d9058e9311b4738c4692bf9febb26c
Arturo Cobos <arturo@co-boss.com>
2016-03-09 00:14:00 -0500

    first try

---
+++ hola_mundo.rb
@@ -0,0 +1,11 @@
+# programa1
+
+puts "Hola mundo"
+puts "hola!!!"
+puts "yei"
+
+# platzi :  `<main>': undefined local variable or method `platzi' for main:Object (NameError)
+
+nombre_del_curso = "Curso Ruby"
+
+puts nombre_del_curso
