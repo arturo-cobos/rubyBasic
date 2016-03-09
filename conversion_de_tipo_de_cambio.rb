ad1693e0a6531fa1d946abd814ed71afa6da71d6
Arturo Cobos <arturo@co-boss.com>
2016-03-09 00:48:22 -0500

    recibir parámetros de consola

--- conversion_de_tipo_de_cambio.rb
+++ conversion_de_tipo_de_cambio.rb
@@ -1,8 +1,12 @@
+puts "Cuantos dolares desea cambiar ?"
+dolares = gets.to_f
 
+puts="A cuanto esta el cambio?"
+cambio = gets.to_f
 
 
 def dollar_to_currency(dollars, tipo_de_cambio = 17.77)
 	return dollars * tipo_de_cambio
 end
 
-puts dollar_to_currency(200,19.24)
+puts dollar_to_currency(dolares,cambio)
